c40c11ce48ec64a5a6c1cf7c2c7a54e281ed48f7
tobslob <kazmobileapp@gmail.com>
2023-11-23 16:56:46 +0000

    add dashboard wireframe

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Dashboard from "../components/Dashboard";
+import "@/app/globals.css";
+
+function MyApp() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export type Show = {
+  id: number;
+  name: string;
+  title: string;
+};
